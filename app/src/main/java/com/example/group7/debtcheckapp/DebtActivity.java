f7fa66ac15aaa3876af04fb07703087bcb7d3c7d
CallWayne <schlueter.niklas@gmail.com>
2016-05-19 17:48:51 +0200

    DebtActivity
    am Layout rumgebastelt

--- app/src/main/java/com/example/group7/debtcheckapp/DebtActivity.java
+++ app/src/main/java/com/example/group7/debtcheckapp/DebtActivity.java
@@ -1,10 +1,19 @@
 package com.example.group7.debtcheckapp;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 
 import com.example.group7.debtcheckapp.Mock.AccountList;
+
+import java.util.Calendar;
 
 public class DebtActivity extends AppCompatActivity {
 
@@ -13,12 +22,20 @@ public class DebtActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debt);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_friends);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_debtor);
         // Create an ArrayAdapter using the string array and a default spinner layout
         UsersAdapter adapter = new UsersAdapter(this, AccountList.getAccList());
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        final Button button = (Button) findViewById(R.id.button_createNewDebt);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
