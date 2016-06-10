05fa8002f59d0b97039244cead00301ceb05c7a4
CallWayne <schlueter.niklas@gmail.com>
2016-06-10 15:10:47 +0200

    JavaDoc Mock fertig

--- app/src/main/java/com/example/group7/debtcheckapp/Mock/OnlineIntegrationServiceInterface.java
+++ app/src/main/java/com/example/group7/debtcheckapp/Mock/OnlineIntegrationServiceInterface.java
@@ -1,28 +1,72 @@
 package com.example.group7.debtcheckapp.Mock;
 
-import com.example.group7.Wsdl2Code.OnlineIntegrationService.debtListResponse;
-import com.example.group7.Wsdl2Code.OnlineIntegrationService.returnCodeResponse;
+/**
+ * Interface für die OnlineIntegration WebService
+ */
+
 import com.example.group7.debtcheckapp.Exceptions.InvalidAddNewDebtException;
 import com.example.group7.debtcheckapp.Exceptions.InvalidLoginException;
-import com.example.group7.debtcheckapp.Exceptions.NoSessionException;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+/**
+ * @author Niklas Schlüter, André Käthner
+ * @version 1.0
+ */
 public interface OnlineIntegrationServiceInterface {
 
+    /**
+     * Methode für das Login
+     * @param email String
+     * @param password String
+     * @return Account
+     * @exception InvalidLoginException
+     */
     public Account login(String email, String password) throws InvalidLoginException;
 
+    /**
+     * Methode für das Logout
+     */
     public void logout();
 
+    /**
+     * Methode für das Registrieren
+     * @param username String
+     * @param email String
+     * @param password String
+     * @return Account
+     * @exception InvalidLoginException
+     */
     public Account signup(String username, String email, String password)throws InvalidLoginException;
 
+    /**
+     * Methode für das adden einer neuen Schuld
+     * @param username String
+     * @param amount BigDecimal
+     * @param reason String
+     * @return Debt
+     * @exception InvalidAddNewDebtException
+     */
     public Debt addNewDebt(String username, BigDecimal amount, String reason) throws InvalidAddNewDebtException;
 
+    /**
+     * GET-Methode für alle Schulden
+     * @return ArrayList
+     */
     public ArrayList<Debt> getAllDebts();
 
+    /**
+     * GET-Methode für alle Forderungen
+     * @return ArrayList
+     */
     public ArrayList<Debt> getAllClaims();
 
+    /**
+     * Methode zum zurückzahlen einer Schuld
+     * @param creditor String
+     * @param amount BigDecimal
+     * @param id int
+     */
     public void payDebt(String creditor, BigDecimal amount, int id);
 
 }
